897f0c0492382fd9386883a055d22db4525724a3
mohraf <amosngok@gmail.com>
2022-01-26 19:33:43 +0300

    iniitial

---
+++ __init__.py
@@ -0,0 +1,10 @@
+def __init__(self, first_name, last_name, phone_number, email):
+  '''
+  __init__ method that helps us defeine properties of our objejcts
+
+  Args:
+      first_name: New contact first name.
+      last_name: new contact last name.
+      number: new contact phone number.
+      email: new contact email address.
+  '''
